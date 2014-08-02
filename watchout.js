b613c62aebf1139c230ec31c3ff117b300d0756d
Lauren <lpappone@gmail.com>
2014-08-01 17:17:28 -0700

    added draggable player

--- watchout.js
+++ watchout.js
@@ -9,7 +9,7 @@ var gameBoard = d3.select('body').append('svg').attr('width', 700)
     .attr('height', 450).style('background-color', 'blue')
     .classed('gameboard', true);
 
-var enemies = gameBoard.selectAll('circle') // circle
+var enemies = gameBoard.selectAll('circle') 
   .data(creatures)
   .enter()
   .append('circle')
@@ -19,14 +19,40 @@ var enemies = gameBoard.selectAll('circle') // circle
   .attr("r", function (d) { return d.radius; })
   .style("fill", function (d) { return d.color; });
 
+
+
+var player = d3.select('.gameboard')
+  .append('circle')
+  .classed('player', true)
+  .attr('cx', 350)
+  .attr('cy', 225)
+  .attr('r', 10)
+
+var dragger = d3.behavior.drag()
+  .on('drag', function() {
+    var x = parseInt(player.attr('cx'), 10);
+    var y = parseInt(player.attr('cy'), 10);
+    console.log('x is', x)
+    player.attr('cx', x + d3.event.dx);
+    player.attr('cy', y + d3.event.dy);
+
+    console.log('dragging', d3.event.dx, d3.event.dy);
+  });
+
+player.call(dragger);
+
 var gameTurn = function() {
   enemies.transition()
   .attr("cx", function() {return Math.random()*700})
   .attr("cy", function() {return Math.random()*450})
-  .duration(1000)
+  .duration(500)
 };
 
-setInterval(gameTurn, 2000);
+var movePlayer = function(dx, dy) {
+
+}
+
+setInterval(gameTurn, 1000);
 
 
 
